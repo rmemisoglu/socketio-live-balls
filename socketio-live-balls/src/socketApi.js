843aaa80e4905f8145d29a3d0779317458d75853
rmemisoglu <r.memisoglu1@gmail.com>
2018-12-19 01:06:46 +0300

    new user event

--- socketio-live-balls/src/socketApi.js
+++ socketio-live-balls/src/socketApi.js
@@ -4,8 +4,25 @@ const io = socketio();
 const socketApi = {};
 socketApi.io = io;
 
+const users = [];
+
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+    socket.on('newUser', (data) => {
+        const defaultData = {
+            id: socket.id,
+            position: {
+                x: 0,
+                y: 0
+            }
+        };
+
+        const userData = Object.assign(data, defaultData);
+        users.push(userData);
+    });
 });
+
+
 
 module.exports = socketApi;
